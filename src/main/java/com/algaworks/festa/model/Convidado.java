337ebf5d7f8c66ff24db99bdd5844843620705fd
wellington.siqueira <wellingtonex@gmail.com>
2017-02-04 19:46:30 -0300

    mudando banco para postgre e preparando para fazer deploy no heroku

--- src/main/java/com/algaworks/festa/model/Convidado.java
+++ src/main/java/com/algaworks/festa/model/Convidado.java
@@ -1,15 +1,19 @@
 package com.algaworks.festa.model;
 
 import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Convidado implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
 	private Long id;
 	private String nome;
 	private Integer quantidadeAcompanhantes;
