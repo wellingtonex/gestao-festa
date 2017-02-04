4c62aaa485bdc7157e248c9d40591a95a345377d
wellington.siqueira <wellingtonex@gmail.com>
2017-02-04 19:26:11 -0300

    projeto redondo com H2

---
+++ src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -0,0 +1,31 @@
+package com.algaworks.festa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.algaworks.festa.model.Convidado;
+import com.algaworks.festa.repository.Convidados;
+
+@Controller
+public class ConvidadosController {
+
+	@Autowired
+	private Convidados convidados;
+
+	@GetMapping("/convidados")
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+		modelAndView.addObject(new Convidado());
+		modelAndView.addObject("convidados", convidados.findAll());
+		return modelAndView;
+	}
+
+	@PostMapping("/convidados")
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
+	}
+}
